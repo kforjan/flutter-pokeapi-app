bb8cda697b9d8ab0e318bc0f32b90164df1ecec2
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-04-21 16:12:49 +0200

    PokemonList model added

--- lib/models/pokemon_list.dart
+++ lib/models/pokemon_list.dart
@@ -1,1 +1,12 @@
-class PokemonList {}
+import 'package:pokemon_api_app/models/pokemon.dart';
+
+class PokemonList {
+  PokemonList(this.pokemons);
+
+  factory PokemonList.fromJson(List<dynamic> json) {
+    final pokemons = json.map((pokemon) => Pokemon.fromMap(pokemon)).toList();
+    return PokemonList(pokemons);
+  }
+
+  final List<Pokemon> pokemons;
+}
