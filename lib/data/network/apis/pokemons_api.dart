a8b9c66bb07cb72e8b9e328c1528d76a67dc479c
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-04-21 17:45:07 +0200

    PokemonsApi added

--- lib/data/network/apis/pokemons_api.dart
+++ lib/data/network/apis/pokemons_api.dart
@@ -1,1 +1,18 @@
-class PokemonsApi {}
+import 'package:pokemon_api_app/constants/endpoints.dart';
+import 'package:pokemon_api_app/data/network/rest_client.dart';
+import 'package:pokemon_api_app/models/pokemon_list.dart';
+
+class PokemonsApi {
+  PokemonsApi(this._restClient);
+
+  final RestClient _restClient;
+
+  Future<PokemonList> getPokemons() async {
+    try {
+      final response = await _restClient.get(Endpoints.getPokemons);
+      return PokemonList.fromJson(response);
+    } catch (e) {
+      throw e;
+    }
+  }
+}
