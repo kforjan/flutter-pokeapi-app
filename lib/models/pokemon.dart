c8ee1b90e6379967004db8cd13d35b7ac00e0ba2
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-04-21 15:46:06 +0200

    Pokemon model added

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,1 +1,18 @@
-class Pokemon {}
+import 'package:flutter/foundation.dart';
+
+class Pokemon {
+  Pokemon({
+    @required this.name,
+    @required this.spriteUrl,
+  });
+
+  factory Pokemon.fromMap(Map<String, dynamic> map) {
+    return Pokemon(
+      name: map['species']['name'],
+      spriteUrl: map['sprites']['front_default'],
+    );
+  }
+
+  final String name;
+  final String spriteUrl;
+}
